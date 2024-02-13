8986e47828470a8e04cad9c3e9eabbb372c2493e
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-02-14 00:59:16 +0100

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__, template_folder='.')
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': '  ',
+    'password': '',
     'database': 'provenderie'
 }
 
@@ -26,7 +26,24 @@ def soumettre_formulaire():
     # Récupérer les données du formulaire
     subject = request.form['subject']
 
-    return 'Sujet choisi: ' + subject
+    # Connexion à la base de données
+    connection = mysql.connector.connect(**db_config)
+    cursor = connection.cursor()
+
+    # Exécuter la requête SQL
+    query = 'SELECT * FROM subjects WHERE namee = %s'
+    cursor.execute(query, (subject,))
+    result = cursor.fetchone()
+
+    # Fermer la connexion
+    cursor.close()
+    connection.close()
+
+    if result:
+        level = result[7]  # Index 7 corresponds to the 'demarrage' value
+        return 'Détails du sujet: ' + str(level)
+    else:
+        return 'Sujet non trouvé.'
 
 
 if __name__ == '__main__':
